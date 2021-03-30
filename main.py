b09b765944a09b6680e55b342f531700bea1d028
Terence <jammy551@hotmail.com>
2021-03-31 02:31:21 +0800

    main.py updated - added topic as condition style.css updated - UI intents.json updated - added topics

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def chatbot_response():
     print("---enters chatbot_response()")
     global first_request
     msg = request.form["msg"]
+    res = ''
     if msg.startswith('my name is'):
         name = msg[11:]
         # tokenize the pattern
@@ -64,11 +65,11 @@ def chatbot_response():
     elif msg.lower() == 'y':
         if first_request:
             print("---continue from previous conversation - y")
-            res = f"Found {first_choice_count} students \n " + first_choice_list
+            res = f"Found {first_choice_count} students \n " + first_choice_list + "\n\n Anything else I can help you now?"
     elif msg.lower() == 'n':
         if first_request:
             print("---continue from previous conversation - n")
-            res = f"Found {all_list_count} students \n " + all_list
+            res = f"Found {all_list_count} students \n " + all_list + "\n\n Anything else I can help you now?"
     else:
         # tokenize the pattern
         msg = clean_up_sentence(msg)
@@ -82,7 +83,9 @@ def chatbot_response():
     if res == "search_student":
         # res = "searching student..."
         # print("searching student...")
-        res = read_csv(msg)
+        res_done, res = read_csv(msg)
+        if res_done == 'yes':
+            res += "\n\n Anything else I can help you now?"
 
     # print("---return:", res)
     return res
@@ -97,8 +100,9 @@ def read_csv(sentence):
     global all_list_count
     condition_course = ""
     condition_recommend = "not recommended"
-    # finding the condition
+    condition_topic = ""
     for w in sentence:
+        # finding the condition_course
         if w == 'c85':
             condition_course = 'c85'
         if w == 'c80':
@@ -111,27 +115,127 @@ def read_csv(sentence):
             condition_course = 'c43'
         if w == 'c54':
             condition_course = 'c54'
+        # find condition_recommend
         if w == 'recommended':
             condition_recommend = 'recommended'
-    print(f'Condition Course: {condition_course}\nCondition Recommended: {condition_recommend}')
+        # find condition_topic
+        if w == 'topic_1':
+            condition_topic = ["5", "7", "9", "10", "14", "17", "19"]
+        if w == 'topic_2':
+            condition_topic = ["2", "8", "12", "14", "16"]
+        if w == 'topic_3':
+            condition_topic = ["0", "1", "3", "4", "13"]
+        if w == 'topic_4':
+            condition_topic = ["6", "11", "15", "18"]
+
+    print(f'Condition Course: {condition_course}'
+          f'\nCondition Recommended: {condition_recommend}'
+          f'\nCondition Topic: {condition_topic}')
     # finding result from file
-    result = ""
-    if condition_course != "":
-        with open('finalfinal.csv', mode='r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            line_count = 0
-            result_count = 0
-            top_20 = 0
-            first_choice_count = 0
-            choice1 = ""
-            choice2 = ""
-            choice3 = ""
-            for row in csv_reader:
-                # print(f"row in csv_reader: {row['UID']}, {row['Choice']}, {row['Course Code']}, {row['Recommended']}, {row['predict_recommend']}")
-                line_count += 1
-                if line_count == 1:
-                    print(f'Column names are {", ".join(row)}')
+    # result = "Please provide a course (dit, dba, dbft, dsf, dcs, cip)"
+    # if condition_course != "":
+    with open('finalfinal.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        result_count = 0
+        top_20 = 0
+        first_choice_count = 0
+        result_complete = 'yes'
+        choice1 = ""
+        choice2 = ""
+        choice3 = ""
+        for row in csv_reader:
+            # print(f"row in csv_reader: {row['UID']}, {row['Choice']}, {row['Course Code']}, {row['Recommended']}, {row['predict_recommend']}")
+            line_count += 1
+            if line_count == 1:
+                print(f'Column names are {", ".join(row)}')
+            # condition_topic check
+            if condition_topic != "":
+                for topic in condition_topic:
+                    if topic == row['predictedTopic']:
+                        # condition_course check
+                        if condition_course.upper() == row['Course Code'].upper():
+                            # condition_recommend check
+                            if condition_recommend == 'recommended':
+                                if row['predict_recommend'] == '1':
+                                    top_20 += 1
+                                    if row['Choice'] == '3':
+                                        if choice3 == "":
+                                            choice3 += (row['UID'].upper() + " Choice: " + row[
+                                                'Choice'] + " Applied: " + row[
+                                                            'Course Code'] + " Predict Recommended: " + row[
+                                                            'predict_recommend'] + " Predict Topic: " + row[
+                                                        'predictedTopic'])
+                                        else:
+                                            choice3 += (" \n " + row['UID'].upper() + " Choice: " + row[
+                                                'Choice'] + " Applied: " + row[
+                                                            'Course Code'] + " Predict Recommended: " + row[
+                                                            'predict_recommend'] + " Predict Topic: " + row[
+                                                        'predictedTopic'] + " ")
+                                    if row['Choice'] == '2':
+                                        if choice2 == "":
+                                            choice2 += (row['UID'].upper() + " Choice: " + row[
+                                                'Choice'] + " Applied: " + row[
+                                                            'Course Code'] + " Predict Recommended: " + row[
+                                                            'predict_recommend'] + " Predict Topic: " + row[
+                                                        'predictedTopic'])
+                                        else:
+                                            choice2 += (" \n " + row['UID'].upper() + " Choice: " + row[
+                                                'Choice'] + " Applied: " + row[
+                                                            'Course Code'] + " Predict Recommended: " + row[
+                                                            'predict_recommend'] + " Predict Topic: " + row[
+                                                        'predictedTopic'] + " ")
+                                    if row['Choice'] == '1':
+                                        if choice1 == "":
+                                            choice1 += (row['UID'].upper() + " Choice: " + row[
+                                                'Choice'] + " Applied: " + row[
+                                                            'Course Code'] + " Predict Recommended: " + row[
+                                                            'predict_recommend'] + " Predict Topic: " + row[
+                                                        'predictedTopic'])
+                                        else:
+                                            choice1 += (" \n " + row['UID'].upper() + " Choice: " + row[
+                                                'Choice'] + " Applied: " + row[
+                                                            'Course Code'] + " Predict Recommended: " + row[
+                                                            'predict_recommend'] + " Predict Topic: " + row[
+                                                        'predictedTopic'] + " ")
+                                        first_choice_count += 1
+                                    result_count += 1
+                            elif condition_recommend == "not recommended":
+                                top_20 += 1
+                                if row['Choice'] == '3':
+                                    if choice3 == "":
+                                        # choice3 += (row['UID'][:-2].upper()+" Choice: "+row['Choice'] + " Applied: " + row['Course Code'] + " Recommended: " + row['predict_recommend'])
+                                        choice3 += (row['UID'].upper() + " Choice: " + row['Choice'] + " Applied: " +
+                                                    row['Course Code'] + " Predict_Topic: " + row[
+                                                        'predictedTopic'])
+                                    else:
+                                        choice3 += (" \n " + row['UID'].upper() + " Choice: " + row[
+                                            'Choice'] + " Applied: " + row['Course Code'] + " Predict_Topic: " +
+                                                    row['predictedTopic'] + " ")
+                                if row['Choice'] == '2':
+                                    if choice2 == "":
+                                        choice2 += (row['UID'].upper() + " Choice: " + row['Choice'] + " Applied: " +
+                                                    row['Course Code'] + " Predict_Topic: " + row[
+                                                        'predictedTopic'])
+                                    else:
+                                        choice2 += (" \n " + row['UID'].upper() + " Choice: " + row[
+                                            'Choice'] + " Applied: " + row['Course Code'] + " Predict_Topic: " +
+                                                    row['predictedTopic'] + " ")
+                                if row['Choice'] == '1':
+                                    if choice1 == "":
+                                        choice1 += (row['UID'].upper() + " Choice: " + row['Choice'] + " Applied: " +
+                                                    row['Course Code'] + " Predict_Topic: " + row[
+                                                        'predictedTopic'])
+                                    else:
+                                        choice1 += (" \n " + row['UID'].upper() + " Choice: " + row[
+                                            'Choice'] + " Applied: " + row['Course Code'] + " Predict_Topic: " +
+                                                    row['predictedTopic'] + " ")
+                                    first_choice_count += 1
+                                result_count += 1
+            else:
+                # condition_course check
                 if condition_course.upper() == row['Course Code'].upper():
+                    # condition_recommend check
                     if condition_recommend == 'recommended':
                         if row['predict_recommend'] == '1':
                             top_20 += 1
@@ -172,25 +276,27 @@ def read_csv(sentence):
                                 choice1 += (" \n "+row['UID'].upper()+" Choice: "+row['Choice'] + " Applied: " + row['Course Code'] + " Predict Recommended: " + row['predict_recommend'] +" ")
                             first_choice_count += 1
                         result_count += 1
-            result = choice1 + "\n " + choice2 + "\n " + choice3
+        result = choice1 + "\n " + choice2 + "\n " + choice3
 
-            # if too many results
-            if top_20 > 20:
-                first_choice_list = choice1
-                all_list = result
-                all_list_count = result_count
-                result = \
-                    f"The list is too long ({result_count} students),\n" \
-                    f" would you like to look for students \n who applied 1st choice?(Y/N)"
+        # if too many results
+        if top_20 > 20:
+            first_choice_list = choice1
+            all_list = result
+            all_list_count = result_count
+            result_complete = 'no'
+            result = \
+                f"The list is too long ({result_count} {condition_course} students),\n" \
+                f" would you like to look for students \n who applied 1st choice?(Y/N)"
 
-            # print(f"---result: {result}")
-            print(f"---all_list : \n{all_list}")
-            print(f'Processed top {top_20} in total.')
-            print(f'Processed {first_choice_count} first choices in total.')
-            print(f'Processed {result_count} results in total.')
-            print(f'Processed {line_count} lines in total.')
+        # print(f"---result: {result}")
+        print(f"---all_list : \n{all_list}")
+        print(f'Processed top {top_20} in total.')
+        print(f'Processed {first_choice_count} first choices in total.')
+        print(f'Processed {result_count} results in total.')
+        print(f'Processed {line_count} lines in total.')
+        print(f'Request Completed? {result_complete}')
 
-    return result
+    return result_complete, result
 
 
 # chat functionalities
@@ -206,7 +312,9 @@ def clean_up_sentence(sentence):
 def check_sentence(sentence):
     print("---enter check_sentence(sentence)", sentence)
     for i, w in enumerate(sentence):
+        # sentence = ['some', 'words', 'here']
         print("---i, w in sentence:", i, w)
+        # setting condition_course if any
         if w == 'dit':
         # Information Technology
             sentence[i] = 'c85'
@@ -225,6 +333,66 @@ def check_sentence(sentence):
         if w == 'dsf':
         # Cybersecurity & Digital Forensics
             sentence[i] = 'c54'
+        '''
+        setting condition_topic
+        '''
+        for s in [
+            # data
+            "data", "information", "datum", "data_point",
+            # coding
+            "cryptography", "coding", "secret_writing", "steganography",
+            "code", "code", "encipher", "cipher", "cypher", "encrypt", "inscribe",
+            "write_in_code", "gull", "dupe", "slang", "befool", "cod", "fool",
+            "put_on", "take_in", "put_one_over", "put_one_across", "tease", "razz",
+            "rag", "cod", "tantalize", "tantalise", "bait", "taunt", "twit", "rally", "ride",
+            # programming
+            "scheduling", "programming", "programing", "programming", "programing",
+            "computer_programming", "computer_programing", "program", "programme", "program", "programme",
+            # python
+            "python", "python", "Python"
+        ]:
+            if w == s:
+                sentence[i] = 'topic_1'
+
+        for s in [
+            # achievement
+            "accomplishment", "achievement"
+        ]:
+            if w == s:
+                sentence[i] = 'topic_2'
+
+        for s in [
+            # participation
+            "engagement", "participation", "involvement", "involution", "participation", "involvement"
+        ]:
+            if w == s:
+                sentence[i] = 'topic_3'
+
+        for s in [
+            # business
+            "business", "concern", "business_concern", "business_organization",
+            "business_organisation", "commercial_enterprise", "business_enterprise",
+            "business", "occupation", "business", "job", "line_of_work", "line",
+            "business", "business", "business", "business", "business_sector", "clientele",
+            "patronage", "business", "business", "stage_business", "byplay",
+            # certificates
+            "certificate", "certification", "credential", "credentials",
+            "security", "certificate", "certificate", "certificate",
+            # challenges
+            "challenge", "challenge", "challenge", "challenge",
+            "challenge", "challenge", "dispute", "gainsay", "challenge",
+            "challenge", "challenge", "take_exception",
+            # cca
+            # values
+            "values", "value", "value", "value", "economic_value", "value",
+            "value", "time_value", "note_value", "value", "value", "prize", "value",
+            "treasure", "appreciate", "respect", "esteem", "value", "prize", "prise",
+            "measure", "evaluate", "valuate", "assess", "appraise", "value", "rate", "value"
+        ]:
+            if w == s:
+                sentence[i] = 'topic_4'
+
+
     print("---return sentence:", sentence)
     return sentence
 
@@ -277,6 +445,26 @@ def getResponse(ints, intents_json):
             break
 
     return result
+
+
+# class ChatBotEae:
+#     def __init__(self, sentence):
+#         self.sentence = sentence
+#
+#     def convert_to_course_code(self, sentence):
+#         pass
+#
+#     def
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
